1a5ca1412228f01329d2b044bf160066b7cea8c5
Johnny <jxhernandez@wpi.edu>
2015-04-22 21:48:13 -0400

    Android Client: - Formatting the ListView, now time and location should appear much nicer - Hint text and hint text color for the New Yell activity

--- app/src/main/java/com/slashyell/jxhernandez/slashyell/YellMessageListAdapter.java
+++ app/src/main/java/com/slashyell/jxhernandez/slashyell/YellMessageListAdapter.java
@@ -1,6 +1,9 @@
 package com.slashyell.jxhernandez.slashyell;
 
 import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,19 +13,25 @@ import android.widget.TextView;
 import com.example.johnny.myapplication.backend.yellMessageApi.model.YellMessage;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by johnny on 4/21/15.
  */
 public class YellMessageListAdapter extends ArrayAdapter<YellMessage> {
 
+    Geocoder geocoder;
+
     public YellMessageListAdapter(Context context, int textViewResourceId) {
         super(context, textViewResourceId);
+        geocoder = new Geocoder(this.getContext(), Locale.getDefault());
     }
 
     public YellMessageListAdapter(Context context, int resource, List<YellMessage> items) {
         super(context, resource, items);
+        geocoder = new Geocoder(this.getContext(), Locale.getDefault());
     }
 
     @Override
@@ -51,11 +60,15 @@ public class YellMessageListAdapter extends ArrayAdapter<YellMessage> {
                 nameField.setText(p.getUserId());
             }
             if (timeField != null) {
-                timeField.setText(p.getDate().toStringRfc3339());
+                timeField.setText(DateUtils.getRelativeDateTimeString(this.getContext(), p.getDate().getValue(), DateUtils.SECOND_IN_MILLIS, DateUtils.WEEK_IN_MILLIS, 0));
             }
             if (locationField != null) {
+
+                List<Address> addresses;
                 try {
-                    locationField.setText(p.getLocation().toPrettyString());
+                    addresses = geocoder.getFromLocation(p.getLocation().getLatitude(), p.getLocation().getLongitude(), 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+                    String location = addresses.get(0).getAddressLine(0);
+                    locationField.setText(location);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
