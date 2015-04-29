fd2c50a0baa6ec8b0e9c3c52ae454a863afc7ec3
Johnny <jxhernandez@wpi.edu>
2015-04-29 18:11:44 -0400

    Android Client: - Clicking items in the list now brings you to the message on the map

--- app/src/main/java/com/slashyell/jxhernandez/slashyell/MainActivity.java
+++ app/src/main/java/com/slashyell/jxhernandez/slashyell/MainActivity.java
@@ -5,15 +5,13 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Criteria;
-import android.location.Location;
 import android.location.LocationManager;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageButton;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -21,31 +19,28 @@ import android.widget.Toast;
 
 import com.androidmapsextensions.GoogleMap;
 import com.androidmapsextensions.MapFragment;
-import com.example.johnny.myapplication.backend.yellMessageApi.YellMessageApi;
+import com.androidmapsextensions.Marker;
 import com.example.johnny.myapplication.backend.yellMessageApi.model.GeoPt;
 import com.example.johnny.myapplication.backend.yellMessageApi.model.YellMessage;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.api.client.extensions.android.http.AndroidHttp;
-import com.google.api.client.extensions.android.json.AndroidJsonFactory;
-import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
-import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
-import com.google.api.client.util.DateTime;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
-import java.util.TimeZone;
+import java.util.Map;
 
 
 public class MainActivity extends Activity implements MessageReceiver{
 
-    private static final float ZOOM_LEVEL = 16f;
+    private static final float ZOOM_LEVEL_NORMAL = 16f;
+    private static final float ZOOM_LEVEL_MESSAGE = 18f;
 
     private MenuItem refreshButton;
     private View refreshView;
     YellMessageWindowAdapter markerAdapter;
+
+    private Map<Long, Marker> idMarkerMap;
 
     private ImageButton newYellButton;
     private GoogleMap map;
@@ -56,6 +51,8 @@ public class MainActivity extends Activity implements MessageReceiver{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        idMarkerMap = new HashMap<Long, Marker>();
 
         // Setting custom action bars
         ActionBar actionBar = getActionBar();
@@ -83,12 +80,23 @@ public class MainActivity extends Activity implements MessageReceiver{
         View emptyView = getLayoutInflater().inflate(R.layout.empty_yell_list,null);
         ((ViewGroup)yellList.getParent()).addView(emptyView);
         yellList.setEmptyView(emptyView);
+        yellList.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+        yellList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                YellMessage message = (YellMessage) yellList.getItemAtPosition(position);
+                Marker messageMarker = idMarkerMap.get(message.getId());
+                messageMarker.showInfoWindow();
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(messageMarker.getPosition(), ZOOM_LEVEL_MESSAGE));
+            }
+        });
 
         GeoPt myLocation = MapUtils.getLocation(gps);
 
         // Initial animation/zoom into users position
         if (myLocation != null)
-            map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(gps.getLastKnownLocation(gps.getBestProvider(new Criteria(), true)).getLatitude(), gps.getLastKnownLocation(gps.getBestProvider(new Criteria(), true)).getLongitude()), ZOOM_LEVEL));
+            map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(gps.getLastKnownLocation(gps.getBestProvider(new Criteria(), true)).getLatitude(), gps.getLastKnownLocation(gps.getBestProvider(new Criteria(), true)).getLongitude()), ZOOM_LEVEL_NORMAL));
 
         //new EndPointAsyncTask().execute();
     }
@@ -121,7 +129,7 @@ public class MainActivity extends Activity implements MessageReceiver{
             // Center the map on the users position
             GeoPt current_location = MapUtils.getLocation(gps);
             if (current_location != null) {
-                map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(current_location.getLatitude(), current_location.getLongitude()), ZOOM_LEVEL));
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(current_location.getLatitude(), current_location.getLongitude()), ZOOM_LEVEL_NORMAL));
             } else {
                 Toast.makeText(this, getResources().getString(R.string.location_unavailable), Toast.LENGTH_LONG);
             }
@@ -146,15 +154,14 @@ public class MainActivity extends Activity implements MessageReceiver{
 
     @Override
     public void onMessagesReceived(List<YellMessage> messages) {
+        idMarkerMap.clear();
         if (messages != null) {
-            Log.d("UPDATING", "WE GOT MAIL");
             ListAdapter newMessageAdapter = new YellMessageListAdapter(this, R.layout.yell_item, messages);
             yellList.setAdapter(newMessageAdapter);
             for (YellMessage message : messages) {
-                MapUtils.addMessageToMap(map, message, markerAdapter);
+                idMarkerMap.put(message.getId(), MapUtils.addMessageToMap(map, message, markerAdapter));
             }
         } else {
-            Log.d("UPDATING", "NOTHING AROUND US");
             messages = new ArrayList<YellMessage>();
             ListAdapter newMessageAdapter = new YellMessageListAdapter(this, R.layout.yell_item, messages);
             yellList.setAdapter(newMessageAdapter);
